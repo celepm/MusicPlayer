eca3188aa7722fdadea049a06c333fb68f175674
Mustafa Celep <mcelep@gmail.com>
2016-10-31 19:33:57 +0100

    Generated Javadoc

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -4,11 +4,15 @@ import com.classes.ViewClass;
 /**
  * 
  * @author celepm
- * The main class is using arguments from the ViewClass to execute the application.
+ * The main class is executing the application.
  *
  */
 public class Main {
 
+	/**
+	 * 
+	 * @param args  * The main method is using arguments from the ViewClass to execute the application.
+	 */
 	public static void main(String[] args) {
 		ViewClass.main(args);
 	}
